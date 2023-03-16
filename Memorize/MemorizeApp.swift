19c2e39ca4baf202aab72d76eb3786a1be33fb0a
Hotae Kim <hotae.kim.94@gmail.com>
2023-03-16 19:34:10 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hotae Kim on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
